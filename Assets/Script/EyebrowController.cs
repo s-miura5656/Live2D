c72f329f20d7f186a63d61176cdcc69f5f3960f9
souta_miura <miura042910@gmail.com>
2019-11-21 11:34:07 +0900

    色々追加

---
+++ Assets/Script/EyebrowController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Core;
+
+public class EyebrowController : MonoBehaviour
+{
+    [SerializeField] private CubismParameter brow_r_form;
+    [SerializeField] private CubismParameter brow_l_form;
+    private float move = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (move <= -1f)
+            {
+                move = -1f;
+            }
+
+            move -= 0.1f;
+        }
+        else
+        {
+            if (move >= 0f)
+            {
+                move = 0f;
+            }
+
+            move += 0.1f;
+        }
+
+        SetParameter(brow_r_form, move);
+        SetParameter(brow_l_form, move);
+    }
+
+    void SetParameter(CubismParameter parameter, float value)
+    {
+        if (parameter != null)
+        {
+            parameter.Value = Mathf.Clamp(value, parameter.MinimumValue, parameter.MaximumValue);
+        }
+    }
+}
---
+++ Assets/Script/SmileController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Core;
+using Live2D.Cubism.Framework;
+
+public class SmileController : MonoBehaviour
+{
+    [SerializeField] private CubismParameter smile_r = null;
+    [SerializeField] private CubismParameter smile_l = null;
+    [SerializeField] private CubismParameter eye_open_r = null;
+    [SerializeField] private CubismParameter eye_open_l =null;
+    private CubismEyeBlinkController eye_blink_script = null;
+    private float move_smile = 0f;
+    private float move_eye = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eye_blink_script = GetComponent<CubismEyeBlinkController>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            eye_blink_script.enabled = false;
+
+            if (move_smile >= smile_r.MaximumValue)
+            {
+                move_smile = smile_r.MaximumValue;
+            }
+
+            if (move_eye <= eye_open_r.MinimumValue)
+            {
+                move_eye = eye_open_r.MinimumValue;
+            }
+
+            move_smile += 0.1f;
+            move_eye -= 0.1f;
+        }
+        else
+        {
+            if (move_smile <= smile_r.MinimumValue)
+            {
+                move_smile = smile_r.MinimumValue;
+                eye_blink_script.enabled = true;
+            }
+
+            if (move_eye >= eye_open_r.MaximumValue)
+            {
+                move_eye = eye_open_r.MaximumValue;
+            }
+
+            move_smile -= 0.1f;
+            move_eye += 0.1f;
+        }
+
+        SetParameter(eye_open_r, move_eye);
+        SetParameter(eye_open_l, move_eye);
+        SetParameter(smile_r, move_smile);
+        SetParameter(smile_l, move_smile);
+    }
+
+    void SetParameter(CubismParameter parameter, float value)
+    {
+        if (parameter != null)
+        {
+            parameter.Value = Mathf.Clamp(value, parameter.MinimumValue, parameter.MaximumValue);
+        }
+    }
+}
